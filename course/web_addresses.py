20dd2bc001d6427d3b6374046b7541f3643ea418
EnkiEnki77 <layman212@gmail.com>
2025-03-18 03:37:25 -0400

    Learned the anatomy of IPv4 and IPv6

---
+++ course/web_addresses.py
@@ -0,0 +1,11 @@
+# Our computer is connected to the internet, a network of millions of other computers also connected to the
+# internet.
+# When we want to communicate with one of the other computers over the network we specify the computer through
+# its IP address aka web address.
+# Each computer has it's own unique IP address, that allows other computers to directly communicate with it.
+
+# The anatomy of an IP address is four numbers between 0 and 255, separated by dots. For example 17.244.27.199.
+# This format of IP is called IPv4
+
+# There's also IPv6 which has 8 numbers separated by colons :. There are way more possible IPv6 addresses.
+
