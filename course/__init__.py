b6c066b912048881a4dffa669eb15d23d6d3818f
EnkiEnki77 <layman212@gmail.com>
2025-03-18 01:37:40 -0400

    Learned what a protocol is

---
+++ course/__init__.py
@@ -0,0 +1,11 @@
+# http is the most popular protocol for exchanging data across the internet.
+
+# A protocol is a set of rules agreed upon between computers across the world that then allow computers to
+# communicate across the internet.
+
+# A URL (uniform resource locator) is the address of another computer or resource on the internet. Part of the URL
+# specifies how to reach the server, and part of it tells the server what resource we want.
+
+# In order to utilize http to request data from a server we need to supply a URL that specifies the server, and
+# what data we're wanting.
+
