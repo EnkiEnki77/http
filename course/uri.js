61def898728baaa215e69672e873daa78f69a8e5
EnkiEnki77 <layman212@gmail.com>
2025-03-19 18:39:47 -0400

    Learned about URI's, and how there are two types, URL's and URN's

---
+++ course/uri.js
@@ -0,0 +1,7 @@
+// URI (Uniform Resource Identifier) is a unique character sequence that identifies a resource that is
+// accessed via the internet.
+
+// URI's have syntax rules, to ensure uniformity. Allowing websites to interpret them in the same way.
+
+// URI's come in two types: URL's (uniform resource locator), and URN's (uniform resource name)
+
